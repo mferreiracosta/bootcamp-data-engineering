6617cbf797317a01cec30f52936393af2e8065f7
mferreiracosta <matheeusferreeira@hotmail.com>
2023-02-11 16:51:51 -0300

    refactor: created read_csv and read_delta

--- processing/job-spark-app-emr.py
+++ processing/job-spark-app-emr.py
@@ -12,3 +12,27 @@ spark = SparkSession \
 # definindo o método de logging da aplicação. use INFO somente em DEV [INFO, ERROR]
 spark.sparkContext.setLogLevel("ERROR")
 
+# cria a função para ler dados em .csv
+def read_csv(bucket, path):
+    # lendo os dados do Data Lake
+    df = spark.read.format("csv") \
+        .option("header", "True") \
+        .option("inferSchema", "True") \
+        .csv(f"{bucket}/{path}")
+
+    # imprime os dados lidos da camada bronze
+    print("\nImprime os dados lidos da bronze:")
+    print(df.show(5))
+
+    # imprime o schema do dataframe
+    print("\nImprime o schema do dataframe lido da bronze:")
+    print(df.printSchema())
+
+    return df
+
+# cria a função para ler dados em delta
+def read_delta(bucket, path):
+    df = spark.read.format("delta") \
+        .load(f"{bucket}/{path}")
+    
+    return df
