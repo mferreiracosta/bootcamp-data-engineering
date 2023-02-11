ea7d4b418a77e29964aae7f44c5c1988910032b5
mferreiracosta <matheeusferreeira@hotmail.com>
2023-02-11 16:43:29 -0300

    refactor: created and setup app job Spark

---
+++ processing/job-spark-app-emr.py
@@ -0,0 +1,11 @@
+from os.path import abspath
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+# setup da aplicação Spark
+spark = SparkSession \
+    .builder \
+    .appName("job-1-spark") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .getOrCreate()
+
